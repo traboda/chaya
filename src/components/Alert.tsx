c29635c3200f5a1c8b8533703d6115c69db5a0ab
rithulr808 <rithulravichnadra@gmail.com>
2023-01-21 07:53:18 +0530

    added alert

---
+++ src/components/Alert.tsx
@@ -0,0 +1,130 @@
+import React, { useContext, useState } from 'react';
+import Button, { ButtonProps } from './Button';
+import DSRContext from '../contexts/DSRContext';
+import clsx from 'clsx';
+
+
+type Alert = {
+  type?: 'success' | 'info' | 'warning' | 'danger' | 'default';
+  variant?: 'filled' | 'outline';
+  id?: string,
+  className?: string,
+  title?: string
+  description?: string;
+  iconClassName?: string,
+  allowDismissal?: boolean,
+  onDismiss?: () => void,
+  primaryButton?: ButtonProps,
+  secondaryButton?: ButtonProps,
+
+};
+
+const getBgByType = (type: string, isDarkTheme?: boolean) => {
+  if (isDarkTheme) {
+    switch (type) {
+      case 'success': return '#025f2b';
+      case 'info': return '#024c6a';
+      case 'warning': return '#907400';
+      case 'danger': return '#7b0707';
+      default: return '#333';
+    }
+  } else {
+    switch (type) {
+      case 'success': return '#dff0d8';
+      case 'info': return '#d9edf7';
+      case 'warning': return '#fcf8e3';
+      case 'danger': return '#fddede';
+      default: return '#eee';
+    }
+  }
+};
+
+const getColorByType = (type: string, isDarkTheme?: boolean) => {
+  if (isDarkTheme) {
+    switch (type) {
+      case 'success': return '#B5FFD9';
+      case 'info': return '#90E0EF';
+      case 'warning': return '#ffeedb';
+      case 'danger': return '#fac8df';
+      default: return '#FFF';
+    }
+  } else {
+    switch (type) {
+      case 'success': return '#3c763d';
+      case 'info': return '#31708f';
+      case 'warning': return '#8a6d3b';
+      case 'danger': return '#c20101';
+      default: return '#111';
+    }
+  }
+};
+
+const Alert = ({
+  type = 'default', variant = 'filled', id, className = '', title, description, iconClassName, allowDismissal = false,
+  onDismiss = () => {}, primaryButton, secondaryButton,
+}: Alert) => {
+
+  const { iconSet }  = useContext(DSRContext);
+  const [hide, setHide] = useState(false);
+  const { isDarkTheme } = useContext(DSRContext);
+  const color = getColorByType(type, isDarkTheme);
+  const borderColor = variant === 'outline' ? getColorByType(type, isDarkTheme ) : undefined;
+  const borderStyle = variant === 'outline' ? 'solid' : undefined;
+  const borderWidth = variant === 'outline' ? '1px' : undefined;
+  const backgroundColor = variant === 'filled' ? getBgByType(type, isDarkTheme) : undefined;
+  const computedClassName = clsx([
+    description ? 'dsr-py-4' : 'dsr-py-2',
+    'dsr-relative dsr-rounded-lg dsr-px-3 dsr-flex dsr-flex-col dsr-gap-2',
+    className,
+  ]);
+
+  return !hide ? (
+      <div
+          id={id}
+          className={computedClassName}
+          style={{ color, borderColor, backgroundColor, borderStyle, borderWidth }}
+      >
+          {allowDismissal && (
+              <div className="dsr-absolute dsr-top-0 dsr-right-0 dsr-pr-3 dsr-pt-2">
+                  <button
+                      title="dismiss"
+                      className="dsr-font-mono dsr-outline-none dsr-font-bold dsr-text-2xl"
+                      onClick={() => { setHide(true); onDismiss(); }}
+                  >
+                      {iconSet?.times?.({ width: 18, height: 18 })}
+                  </button>
+              </div>
+          )}
+          {title && (
+              <h4 className={description ? 'dsr-text-xl dsr-font-semibold' : 'dsr-text-lg'}>
+                  {iconSet?.title?.({ width: 18, height: 18 })}
+                  {iconClassName && <i className={iconClassName} />}
+                  {title}
+              </h4>
+          )}
+          {description && <p className="dsr-text-lg">{description}</p>}
+          {(primaryButton || secondaryButton) && (
+          <div className="dsr-flex dsr-items-center">
+              {primaryButton &&
+              <div className="dsr-mr-2">
+                  <Button
+                      {...primaryButton}
+                      color="primary"
+                      className="dsr-text-base"
+                  />
+              </div>}
+              {secondaryButton &&
+              <div>
+                  <Button
+                      {...secondaryButton}
+                      color="secondary"
+                      className="dsr-text-base"
+                  />
+              </div>}
+          </div>
+          )}
+      </div>
+  ) : <div />;
+};
+
+export default Alert;
