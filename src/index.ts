ac5cc32ef9b533ba8634113a4e1068ce4874e021
Rithul R <92503058+rithulr808@users.noreply.github.com>
2023-01-22 01:25:22 +0530

    added Avatar (#23)
    Co-authored-by: rithulr808 <rithulravichnadra@gmail.com>
    Co-authored-by: Advaith A J <advaitharunjeena@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@ export { default as DSRContextProvider } from './components/DSRContextProvider';
 export { default as Icon } from './components/Icon';
 export { default as Card } from './components/Card';
 export { default as Modal } from './components/Modal';
+export { default as Alert } from './components/Alert';
+export { default as Avatar } from './components/Avatar';
 export { default as Button } from './components/Button';
 export { default as TextInput } from './components/TextInput';
 export { default as Accordion } from './components/Accordion';
 export { default as SearchBox } from './components/SearchBox';
 export { default as AccordionGroup } from './components/AccordionGroup';
-export { default as Alert } from './components/Alert';
 
 // hooks
 export { default as useCounter } from './hooks/useCounter';
