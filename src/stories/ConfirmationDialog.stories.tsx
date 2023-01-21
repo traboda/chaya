c7af548078680eb86c1510d56163b6b148163166
rithulr808 <rithulravichnadra@gmail.com>
2023-01-22 03:44:00 +0530

    added ConfirmationDialog

---
+++ src/stories/ConfirmationDialog.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Meta, Story, addDecorator } from '@storybook/react';
+import { ConfirmationDialog } from '../index';
+import ThemeContextDecorator from './ThemeContextDecorator';
+
+addDecorator(ThemeContextDecorator);
+
+const meta: Meta = {
+  title: 'Overlays/Confirmation Dialog',
+  component: ConfirmationDialog,
+};
+
+export default meta;
+
+const Template: Story = args => {
+
+  const [isOpen, setIsOpen] = React.useState(args.isOpen);
+
+  React.useEffect(() => {
+    setIsOpen(args.isOpen);
+  }, [args.isOpen]);
+
+  return (
+      <div>
+          <button onClick={() => setIsOpen(true)}>Open</button>
+          <ConfirmationDialog
+              isOpen={isOpen}
+              onCancel={() => setIsOpen(false)}
+              onConfirm={() => setIsOpen(false)}
+              {...args}
+          />
+      </div>
+  );
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  isOpen: true,
+};
