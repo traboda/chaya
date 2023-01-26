801b36133a4002dc96abab1d89a8cdf3576fed43
abhikrishnaram <abhikrishnaram88@gmail.com>
2023-01-26 14:54:10 +0530

    radio-button v1

---
+++ src/components/Radio/Radio.tsx
@@ -0,0 +1,59 @@
+import React, { useContext } from 'react';
+import clsx from 'clsx';
+
+import DSRContext from '../../contexts/DSRContext';
+
+import styled from './radio.module.scss';
+
+export type RadioOptionType = {
+  label: string,
+  value: string,
+  disabled?: boolean,
+};
+
+type RadioButtonProps = {
+  onChange: (value: any) => void,
+  option: RadioOptionType,
+  value: RadioOptionType | string,
+  color?: string,
+  size?: string,
+  isDisabled?: boolean,
+  spacing?: string,
+};
+
+const Radio = ({ option, value, onChange, color, size, isDisabled, spacing }: RadioButtonProps) => {
+
+  const { isDarkTheme } = useContext(DSRContext);
+  const isChecked = typeof value === 'string' ? value === option.value : value.value === option.value;
+
+  return (
+      <div
+          className={clsx([ 'dsr-inline-flex dsr-items-center dsr-cursor-pointer dsr-relative', spacing ])}
+          onClick={() => onChange(option.value)}
+      >
+          <input
+              type="radio"
+              name={option.value}
+              value={option.value}
+              checked={isChecked}
+              disabled={isDisabled || option.disabled}
+              className={clsx([
+                'dsr-border-0 dsr-border-none dsr-h-px dsr-w-px dsr-p-0 dsr-whitespace-nowrap',
+                'dsr-overflow-hidden dsr-absolute -dsr-m-1',
+              ])}
+              style={{ clip: 'rect(0px, 0px, 0px, 0px)' }}
+          />
+          <span
+              className={clsx([
+                'dsr-inline-flex dsr-items-center dsr-justify-center dsr-flex-shrink-0',
+                'dsr-transition-shadow dsr-duration-normal dsr-border dsr-border-2 dsr-border-none dsr-rounded-full',
+                'dsr-border-inherit dsr-text-white', size, isChecked && styled.radioButton,
+                isChecked ? color : isDarkTheme ? 'dsr-bg-white/20' : 'dsr-bg-gray-500/20',
+              ])}
+          />
+          <span className="dsr-ml-2">{option.label}</span>
+      </div>
+  );
+};
+
+export default Radio;
---
+++ src/components/Radio/RadioGroup.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import clsx from 'clsx';
+
+import Radio, { RadioOptionType } from './Radio';
+
+type RadioColor = 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'default';
+export type RadioSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+type RadioGroupType = {
+  onChange: (value: string) => void,
+  value: RadioOptionType | string,
+  options: RadioOptionType[],
+  color?: RadioColor, 
+  size?: RadioSize, 
+  isDisabled?: boolean,
+  alignment?: 'horizontal' | 'vertical',
+};
+
+const colors = {
+  'primary': 'dsr-bg-primary',
+  'secondary': 'dsr-bg-secondary',
+  'success': 'dsr-bg-green-500',
+  'danger': 'dsr-bg-red-500',
+  'warning': 'dsr-bg-yellow-400',
+  'default': 'dsr-bg-gray-500/70',
+};
+
+const sizes = {
+  'xs': 'dsr-h-2 dsr-w-2',
+  'sm': 'dsr-h-3 dsr-w-3',
+  'md': 'dsr-h-4 dsr-w-4',
+  'lg': 'dsr-h-5 dsr-w-5',
+  'xl': 'dsr-h-6 dsr-w-6',
+};
+
+const RadioGroup = ({ onChange, value, options, color = 'primary', size = 'md', isDisabled = false, alignment = 'vertical' }: RadioGroupType) => {
+
+  return (
+      <div className={clsx([ 'dsr-flex', alignment === 'vertical' ? 'dsr-flex-col' : 'dsr-flex-row'])}>
+          {options.map((option, index) => (
+              <Radio
+                  key={index}
+                  option={option}
+                  value={value}
+                  color={colors[color]}
+                  size={sizes[size]}
+                  spacing={alignment === 'vertical' ? 'dsr-my-2' : 'dsr-mx-4'}
+                  onChange={onChange}
+                  isDisabled={isDisabled}
+              />
+          ))}
+      </div>
+  );
+};
+
+export default RadioGroup;
