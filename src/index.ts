b713e205de079f37e9d4cc1e5515bb759fc23d88
Rithul R <92503058+rithulr808@users.noreply.github.com>
2023-01-27 23:49:32 +0530

    Added drawer (#46)
    * Added drawer
    
    * fixes drawer
    
    * made changes in drawer.module.scss
    
    ---------
    
    Co-authored-by: rithulr808 <rithulravichnadra@gmail.com>
    Co-authored-by: Advaith A J <advaitharunjeena@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export { default as Modal } from './components/Modal';
 export { default as Alert } from './components/Alert';
 export { default as Badge } from './components/Badge';
 export { default as Radio } from './components/Radio';
+export { default as Drawer } from './components/Drawer';
 export { default as Switch } from './components/Switch';
 export { default as Avatar } from './components/Avatar';
 export { default as Button } from './components/Button';
