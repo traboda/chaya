289964d5484c3c05c5f79e45bf117c3bf8ee5091
Pranavkrishnan007 <pranavk0217@gmail.com>
2023-01-28 00:22:49 +0530

    requested changes

--- src/components/PageNavigator.tsx
+++ src/components/PageNavigator.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 
 import Button from './Button';
 import SimpleSelect from './SimpleSelect';
-import Icon, { IconInputType } from './Icon';
+import Icon from './Icon';
 
 type PageNavigator = {
   totalCount: number,
@@ -16,18 +16,12 @@ type PageNavigator = {
   setPage: (no: number) => void,
   id?: string,
   className?: string,
-  icons?: {
-    next?: IconInputType,
-    prev?: IconInputType,
-    start?: IconInputType,
-    end?: IconInputType,
-  }
 };
 
 
 const PageNavigator = ({
   totalCount, itemsPerPage, setItemsPerPage, id, className = '', hideItemsPerPage = false, page, setPage,
-  icons: iconInput, showControls = true, showEdges = true,
+  showControls = true, showEdges = true,
 }: PageNavigator) => {
 
 
@@ -37,9 +31,7 @@ const PageNavigator = ({
     let list = [];
     if (length > 1) {
       if (length < 6)
-        Array.from({ length }, (_item, i) => {
-          list.push(i + 1);
-        });
+        Array(length).fill(0).map((_, i) => list.push(i + 1));
       else if (page > 2 && page + 2 < length)
         for (let i = page - 2; i <= (page + 2); i++)
           list.push(i);
@@ -62,71 +54,69 @@ const PageNavigator = ({
             className,
           ])}
       >
-          <div style={{ userSelect: 'none' }}>
+          <div className="dsr-select-none">
               <div className="dsr-flex dsr-items-stretch">
                   {(showEdges && page > 2) && (
                       <Button className="dsr-w-12 dsr-flex dsr-items-center dsr-justify-center dsr-mx-1" onClick={() => setPage(1)}>
-                          <Icon icon={iconInput?.start ?? 'chevronsLeft'} size={18} />
+                          <Icon icon={'chevronsLeft'} size={18} />
                       </Button>
                   )}
                   {(showControls && page > 1) && (
                       <Button className="dsr-w-12 dsr-flex dsr-items-center dsr-justify-center dsr-mx-1" onClick={() => setPage(page - 1)}>
-                          <Icon icon={iconInput?.prev ?? 'chevronLeft'} size={18} />
+                          <Icon icon={'chevronLeft'} size={18} />
                       </Button>
                   )}
-                  {getPageNo().map((item, index) =>
-                    (
-                        <Button
-                            variant={page === item ? 'solid' : 'outline'}
-                            color={page === item ? 'contrast' : 'secondary'}
-                            disabled={page === item}
-                            key={`page_${item}_${index}`}
-                            className={clsx([
-                              'dsr-w-12 dsr-mx-1',
-                              page === item ? 'active' : '',
-                            ])}
-                            onClick={() => setPage(item)}
-                        >
-                            {item}
-                        </Button>
-                    ),
-                  )}
+                  {getPageNo().map((item, index) => (
+                      <Button
+                          variant={page === item ? 'solid' : 'outline'}
+                          color={page === item ? 'contrast' : 'secondary'}
+                          disabled={page === item}
+                          key={`page_${item}_${index}`}
+                          className={clsx([
+                            'dsr-w-12 dsr-mx-1',
+                          ])}
+                          onClick={() => setPage(item)}
+                      >
+                          {item}
+                      </Button>
+                  ))}
                   {(showControls && !(page + 1 >= length)) && (
                       <Button className="dsr-w-12 dsr-flex dsr-items-center dsr-justify-center dsr-mx-1" onClick={() => setPage(page + 1)}>
-                          <Icon icon={iconInput?.next ?? 'chevronRight'} size={18} />
+                          <Icon icon={'chevronRight'} size={18} />
                       </Button>
                   )}
                   {(showEdges && (page + 1 < length)) && (
                       <Button className="dsr-w-12 dsr-flex dsr-items-center dsr-justify-center dsr-mx-1" onClick={() => setPage(length)}>
-                          <Icon icon={iconInput?.end ?? 'chevronsRight'} size={18} />
+                          <Icon icon={'chevronsRight'} size={18} />
                       </Button>
                   )}
               </div>
               {!hideItemsPerPage &&
-              <div className="dsr-flex dsr-items-center dsr-justify-center dsr-mt-3">
-                  <div className="dsr-mr-1" style={{ width: '38px' }}>
-                      <SimpleSelect
-                          value={itemsPerPage}
-                          onChange={(n) => {
-                            setPage(1); // @ts-ignore
-                            setItemsPerPage(parseInt(n));
-                          }}
-                          name="items_per_page"
-                          className="dsr-p-1 dsr-text-sm dsr-text-center"
-                          removeSVG={true}
-                          required
-                          options={[
-                            { label: '10', value: 10 },
-                            { label: '20', value: 20 },
-                            { label: '30', value: 30 },
-                            { label: '50', value: 50 },
-                            { label: '100', value: 100 },
-                          ]}
-                      />
+                  <div className="dsr-flex dsr-items-center dsr-justify-center dsr-mt-3">
+                      <div className="dsr-mr-1 dsr-w-10">
+                          <SimpleSelect
+                              value={itemsPerPage}
+                              onChange={(n) => {
+                                setPage(1);
+                                setItemsPerPage(typeof n === 'number' ? n : parseInt(n));
+                              }}
+                              name="items_per_page"
+                              className="dsr-p-1 dsr-text-sm dsr-text-center"
+                              removeSVG={true}
+                              required
+                              options={[
+                                { label: '10', value: 10 },
+                                { label: '20', value: 20 },
+                                { label: '30', value: 30 },
+                                { label: '50', value: 50 },
+                                { label: '100', value: 100 },
+                              ]}
+                          />
+                      </div>
+                      {' '}
+                      items per page
                   </div>
-                  {' '}
-                  items per page
-              </div>}
+              }
           </div>
       </div>
   );
