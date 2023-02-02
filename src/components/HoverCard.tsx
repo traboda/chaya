ec2cbb40eda80add5a6c05107dc5bd0bbb85f3e7
AbhiramKrishna <66553902+abhikrishnaram@users.noreply.github.com>
2023-02-02 21:10:20 +0530

    HoverCard v0.8 (#83)
    * HoverCard v0.8
    
    * HoverCard v0.8.2
    
    * HoverCard v1.0
    
    * HoverCard v1.2

---
+++ src/components/HoverCard.tsx
@@ -0,0 +1,56 @@
+import React, { ReactNode, useContext, useMemo } from 'react';
+import * as RadixHoverCard from '@radix-ui/react-hover-card';
+import clsx from 'clsx';
+import { nanoid } from 'nanoid';
+import Color from 'color';
+
+import DSRContext from '../contexts/DSRContext';
+
+type HoverCardProps = {
+  id?: string,
+  className?: string,
+  children: ReactNode,
+  cardRenderer: ReactNode,
+  minWidth?: string | number,
+  maxWidth?: string | number,
+};
+
+const HoverCard = ({ children, cardRenderer, id, className, minWidth = 250, maxWidth = 350 }: HoverCardProps) => {
+
+  const { isDarkTheme, theme } = useContext(DSRContext);
+
+  const hoverCardID = useMemo(() => id && id.length > 1 ? id : `hovercard-${nanoid()}`, [id]);
+  
+  const cardBackground = useMemo(() => {
+    const background = Color(theme?.background);
+    return isDarkTheme ? background.lighten(0.8).fade(0.2).toString() : background.lighten(0.4).fade(0.2).toString();
+  }, [theme]);
+
+  return (
+      <RadixHoverCard.Root openDelay={100} closeDelay={100}>
+          <RadixHoverCard.Trigger>
+              {children}
+          </RadixHoverCard.Trigger>
+          <RadixHoverCard.Portal>
+              <RadixHoverCard.Content
+                  className={clsx([
+                    'hover-card-content',
+                    'dsr-rounded-lg dsr-shadow-lg dsr-text-color dsr-backdrop-blur-md dsr-transform-gpu',
+                    className,
+                  ])}
+                  style={{
+                    background: cardBackground,
+                    minWidth,
+                    maxWidth,
+                  }}
+                  sideOffset={5}
+                  id={hoverCardID}
+              >
+                  {cardRenderer}
+              </RadixHoverCard.Content>
+          </RadixHoverCard.Portal>
+      </RadixHoverCard.Root>
+  );
+};
+
+export default HoverCard;
---
+++ src/stories/HoverCard.stories.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { addDecorator, Meta, Story } from '@storybook/react';
+
+import { Button, HoverCard } from '../index';
+
+import ThemeContextDecorator from './ThemeContextDecorator';
+
+addDecorator(ThemeContextDecorator);
+
+const meta: Meta = {
+  title: 'Content Handlers/HoverCard',
+  component: HoverCard,
+  argTypes: {
+    children: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  parameters: {
+    backgrounds: {},
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+
+const Card = () => (
+    <figure className="dsr-rounded-lg dsr-p-8">
+        <img
+            className="dsr-w-24 dsr-h-24 dsr-rounded-full dsr-mx-auto"
+            src="https://fakeface.rest/face/view"
+            alt=""
+            width="384"
+            height="512"
+        />
+        <div className="dsr-pt-6 dsr-text-center dsr-space-y-4">
+            <blockquote>
+                <p className="dsr-text-lg dsr-font-medium">
+                    “Lorem ipsum dolor sit amet, ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.”
+                </p>
+            </blockquote>
+            <figcaption className="dsr-font-medium">
+                <div className="dsr-text-primary">
+                    Jane Doe
+                </div>
+                <div className="dsr-text-color dsr-opacity-50">
+                    Staff Engineer, Algolia
+                </div>
+            </figcaption>
+        </div>
+    </figure>
+
+);
+
+const Template: Story = args => (
+    <div
+        className="dsr-fixed dsr-top-1/2 dsr-left-0 dsr-w-full dsr-h-full"
+        style={{ background: 'url(https://source.unsplash.com/random/1920x1080) center / cover no-repeat' }}
+    >
+        <div className="dsr-fixed dsr-top-0 dsr-left-0">
+            <HoverCard cardRenderer={<Card />} {...args}>
+                <Button className="text-white">Hover Top-Left</Button>
+            </HoverCard>
+        </div>
+        <div className="dsr-fixed dsr-top-0 dsr-right-0">
+            <HoverCard cardRenderer={<Card />} {...args}>
+
+                <button className="text-white">Hover Here</button>
+            </HoverCard>
+        </div>
+        <div className="dsr-fixed dsr-bottom-0 dsr-right-0">
+            <HoverCard cardRenderer={<Card />} {...args}>
+                <button>Hover Here</button>
+            </HoverCard>
+        </div>
+        <div className="dsr-fixed dsr-bottom-0 dsr-left-0">
+            <HoverCard
+                className="dsr-text-white !dsr-bg-amber-300"
+                children={<div className="text-white">Hover Here</div>}
+                cardRenderer={<Card />}
+                {...args}
+            />
+        </div>
+        <div className="dsr-fixed dsr-top-3/4 dsr-left-1/2">
+            <HoverCard
+                children={<div className="text-white">Hover Here</div>}
+                cardRenderer={<Card />}
+                {...args}
+            />
+        </div>
+
+    </div>
+);
+
+export const Default = Template.bind({});
+
+Default.args = {};
