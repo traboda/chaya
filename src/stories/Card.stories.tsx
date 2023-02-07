f6de8074e97e0b4c5e41d0d3aef61b9f5ea06886
Ashwin S Shenoy <aswinshenoy65@gmail.com>
2023-02-07 22:00:42 +0530

    Consistency check stories

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 
-import { Card, Button } from '../index';
+import {
+  Card,
+  Button,
+  TextInput,
+  PinInput,
+  SearchBox,
+  SelectorButton,
+  SimpleSelect,
+  Switch,
+  TagSelector, PageNavigator, Badge,
+} from '../index';
 
 const meta: Meta = {
   title: 'Basic Elements/Card',
@@ -21,7 +31,6 @@ const meta: Meta = {
 export default meta;
 
 const Template: Story = args => (
-    // @ts-ignore
     <Card {...args}>
         {args.children}
         <Card>
@@ -65,3 +74,88 @@ OutlinedCard.args = {
   description: 'The following settings can be customized to make the card even more awesome.',
   className: 'dsr-px-20 dsr-py-12 dsr-m-10',
 };
+
+
+const HeightPreviewCardTemplate: Story = () => (
+    // @ts-ignore
+    <div background="#111">
+        <div className="dsr-flex dsr-flex-wrap dsr-items-end">
+            <div className="dsr-w-1/4 dsr-p-1">
+                {/*// @ts-ignore*/}
+                <TextInput label="hello" />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                {/*// @ts-ignore*/}
+                <PinInput labels={{ label: 'hello' }} />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                <SearchBox keyword="abc" setKeyword={() => {}} />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                <SelectorButton name="hello" options={[{ label: 'hello', value: 'hello' }]} />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                {/*// @ts-ignore*/}
+                <TextInput label="hello" type="password" />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                {/*// @ts-ignore*/}
+                <PinInput variant="classic" labels={{ label: 'hello' }} />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                <SimpleSelect value="hello" name="hello" options={[{ label: 'hello', value: 'hello' }]} labels={{ label: 'hello' }} />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                {/*// @ts-ignore*/}
+                <Switch label="hello" />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                {/*// @ts-ignore*/}
+                <TextInput label="hello" type="textarea" rows={1} />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                {/*// @ts-ignore*/}
+                <TagSelector labels={{ title: 'hello' }} value="hello" options={[{ label: 'hello', value: 'hello' }, { label: 'bye', value: 'bye' }]} />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                {/*// @ts-ignore*/}
+                <TextInput label="hello" />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                <Button>
+                    hi elllo
+                </Button>
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                <PageNavigator
+                    page={1}
+                    itemsPerPage={30}
+                    totalCount={100}
+                    hideItemsPerPage
+                />
+            </div>
+            <div className="dsr-w-1/8 dsr-p-1">
+                <Button variant="outline">
+                    hi elllo
+                </Button>
+            </div>
+            <div className="dsr-w-1/8 dsr-p-1">
+                <Badge variant="outline" size="md">
+                    hi elllo
+                </Badge>
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                {/*// @ts-ignore*/}
+                <TextInput label="hello" />
+            </div>
+            <div className="dsr-w-1/4 dsr-p-1">
+                <Card className="dsr-p-2" variant="outline">
+                    j1
+                </Card>
+            </div>
+        </div>
+    </div>
+);
+export const HeightPreviewCard = HeightPreviewCardTemplate.bind({});
+
+HeightPreviewCard.args = {};
